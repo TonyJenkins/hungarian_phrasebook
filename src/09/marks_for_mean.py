5dcdc063a05ca103b92437a3999e07f5f562c41d
Tony Jenkins <tony@tony-jenkins.co.uk>
2023-09-01 15:10:17 +0100

    Code tweak

--- src/09/marks_for_mean.py
+++ src/09/marks_for_mean.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     marks = []
 
-    for count in range(NUMBER_OF_MARKS):
+    for _ in range(NUMBER_OF_MARKS):
         next_mark = int(input('Enter the next mark: '))
         marks.append(next_mark)
 
